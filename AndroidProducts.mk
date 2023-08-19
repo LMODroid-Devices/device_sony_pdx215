c7d62bac16495f6d7e85e6d1dd2e4c75736e9d72
LMO GerritBot <gerrit@libremobileos.com>
2023-08-19 11:26:35 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_pdx215.mk
+    $(LOCAL_DIR)/lmodroid_pdx215.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_pdx215-user \
-    lineage_pdx215-userdebug \
-    lineage_pdx215-eng
+    lmodroid_pdx215-user \
+    lmodroid_pdx215-userdebug \
+    lmodroid_pdx215-eng
